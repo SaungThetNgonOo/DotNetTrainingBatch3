69184a059cb5334eaf9684dfb44db7427554ce0d
Saung Thet Ngon Oo <saung1872000@gmail.com>
2024-03-18 01:13:14 +0630

    [Complete] ASP.NetCore MVC-Blog Edit,Create,Update,Delete

---
+++ DotNetTrainingBatch3.MvcApp/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using DotNetTrainingBatch3.MvcApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTrainingBatch3.MvcApp;
+
+public class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        SqlConnectionStringBuilder sqlconnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "TestDb",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true
+        };
+        optionsBuilder.UseSqlServer(sqlconnectionStringBuilder.ConnectionString);
+    }
+
+    public DbSet<BlogModel> Blogs { get; set; }
+}
---
+++ DotNetTrainingBatch3.MvcApp/Controllers/BlogApiController.cs
@@ -0,0 +1,75 @@
+﻿using DotNetTrainingBatch3.MvcApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetTrainingBatch3.MvcApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogApiController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public BlogApiController()
+        {
+            _db = new AppDbContext();
+        }
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            List<BlogModel> lst = _db.Blogs.OrderByDescending(x => x.BlogID).ToList();
+            return Ok(lst);
+        }
+        [HttpGet(template: "{id}")]
+        public IActionResult GetBlog(int id)
+        {
+            BlogModel? item = _db.Blogs.FirstOrDefault(item => item.BlogID == id);
+            if (item is null)
+            {
+                return NotFound("No data found.");
+            }
+            return Ok(item);
+        }
+        [HttpPost]
+        public IActionResult CreateBlog(BlogModel blog)
+        {
+            _db.Blogs.Add(blog);
+            int result = _db.SaveChanges();
+            string message = result > 0 ? "Saving Successful." : "Saving failed.";
+            return Ok(message);
+        }
+        //[HttpGet]
+        //public IActionResult GetBlogs()
+        //{
+        //    return Ok("GetBlogs");
+        //}
+        [HttpPut(template: "{id}")]
+        public IActionResult UpdateBlog(int id, BlogModel blog)
+        {
+            BlogModel? item = _db.Blogs.FirstOrDefault(item => item.BlogID == id);
+            if (item is null)
+            {
+                return NotFound("No Data Found.");
+            }
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+            int result = _db.SaveChanges();
+
+            string message = result > 0 ? "Updating successful." : "Updating Failed.";
+            return Ok(message);
+        }
+        [HttpDelete(template: "{id}")]
+        public IActionResult DeleteBlog(int id)
+        {
+            BlogModel? item = _db.Blogs.FirstOrDefault(item => item.BlogID == id);
+            if (item is null)
+            {
+                return NotFound("No data found.");
+            }
+            _db.Blogs.Remove(item);
+            int result = _db.SaveChanges();
+            string message = result > 0 ? "Deleting Successful." : "Deleting Failed.";
+            return Ok(message);
+        }
+    }
+}
